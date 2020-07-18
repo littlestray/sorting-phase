48402df16b885d25804c859a6a55b6b17c723825
Chris Baldys <36646247+littlestray@users.noreply.github.com>
2020-07-18 18:21:03 -0400

    giving up on lua

--- midisort-merge-ascending.lua
+++ midisort-merge-ascending.lua
@@ -43,7 +43,10 @@ Ascending
 -- end
 
 -- mergeCounter = 0;
+--------------------------------------------------------------------------FIELDS
 
+breadCrumbs = {}
+breadCrumbsIdx = 1;
 
 -----------------------------------------------------------------------FUNCTIONS
 
@@ -64,11 +67,31 @@ function printNotesArray(notes)
 
 end
 
+function breadCrumbsArray(a, b)
+  
+        
+                                  -- LEFT IN FOR RECURSION TESTING
+      for i = 1, #a, 1 do
+      
+        breadCrumbs[breadCrumbsIdx] = a[i]
+        breadCrumbsIdx = 1 + breadCrumbsIdx
+      
+      end
+      
+      for i = 1, #b, 1 do
+      
+      breadCrumbs[breadCrumbsIdx] = b[i]
+      breadCrumbsIdx = 1 + breadCrumbsIdx
+      
+      end
+
+end
+
 function table.slice(table, start, stop)
 
   if start == nil then start = 1 end
   if stop  == nil then stop = #table end
-  local output = {};
+  output = {};
   local outputIdx = 1;
 
   for i = 1, #table, 1 do
@@ -79,16 +102,41 @@ function table.slice(table, start, stop)
     end
 
   end
-
+  
   return output
 
 end
+
+function table.concat(a,b)
+
+    local output = {}
+    local outputIdx = 1;
+                                -- LEFT IN FOR RECURSION TESTING
+    for i = 1, #a, 1 do
+    
+      output[outputIdx] = a[i]
+      outputIdx = 1 + outputIdx
+    
+    end
+    
+    for i = 1, #b, 1 do
+    
+    output[outputIdx] = b[i]
+    outputIdx = 1 + outputIdx
+    
+    end
+    
+    return output
+    
+end
+
 
 function table.combine(a, b)
 
   local output = {}
   local outputIdx = 1;
-
+ 
+  --[[                               LEFT IN FOR RECURSION TESTING
   for i = 1, #a, 1 do
   
     output[outputIdx] = a[i]
@@ -102,12 +150,62 @@ function table.combine(a, b)
   outputIdx = 1 + outputIdx
   
   end
+  ]]
+  
+  i = 1;
+  j = 1;
+ 
+  while i <= (#a + 1) and j <= #b + 1 do
+  
+    --reaper.ShowConsoleMsg(a[i][7])
+    --reaper.ShowConsoleMsg(b[j][7]);
+    reaper.ShowConsoleMsg(i)
+    reaper.ShowConsoleMsg(j)
+   
+    if (i < (#a + 1) and j < (#b + 1) and a[i][7] > b[j][7]) then
+      
+       
+      output[outputIdx] = a[i]
+      outputIdx = 1 + outputIdx
+    
+      i = i + 1
+      reaper.ShowConsoleMsg("a")
+    elseif (i < (#a + 1) and j < (#b + 1) and a[i][7] < b[j][7]) then
+    
+      output[outputIdx] = b[j]
+      
+      outputIdx = 1 + outputIdx
+  
+      j = j + 1
+      reaper.ShowConsoleMsg("b")
+    elseif (i == (#a + 1)) and (j < (#b + 1))  then
+      output[outputIdx] = b[j]
+      outputIdx = 1 + outputIdx
+        
+      j = j + 1
+      reaper.ShowConsoleMsg("c")
+    elseif (j == (#b + 1))  and (i < (#a + 1)) then
+    
+      output[outputIdx] = a[i]
+      outputIdx = 1 + outputIdx
+        
+      i = i + 1
+      reaper.ShowConsoleMsg("d")
+      
+    else
+      i = i + 1
+      j = j + 1
+    end
+  
+    --printNotesArray(output)
+    
+  end
+  
   
   return output
 
 
 end
-
 
 --------------------------------------------------------------------------------
 
@@ -116,7 +214,7 @@ reaper.ShowConsoleMsg("sorting-phase-merge\n");
 --reaper.ShowConsoleMsg("#0\n");
 
 -- copies the midiItem
--- reaper.Main_OnCommand(41295, 1);
+reaper.Main_OnCommand(41295, 1);
 
 target = reaper.GetSelectedMediaItem(0, 0);
 if target == nil then reaper.ShowConsoleMsg("no item selected\n"); return end
@@ -144,25 +242,84 @@ reaper.ShowConsoleMsg("\n")
 
 --printNotesArray(table.slice(notes, 0, 8))
 
-function mergesort(list)
+function mergesort(list, ind)
+
+  local tmp = 0;
+  
+  local index = ind or tmp
   
   local s=math.floor(#list/2)
-  reaper.ShowConsoleMsg("mergesort -- size: " .. #list .. " s: " .. s .. " ")
+  reaper.ShowConsoleMsg("\nmergesort -- index: " .. index .. " size: " .. #list .. " s: " .. s .. " \n")
 
   if #list == 1 then  return list end
   
-  return merge(mergesort(table.slice(list,1,s)), mergesort(table.slice(list,s+1)))
+  return merge(mergesort(table.slice(list,1,s), index + 1), mergesort(table.slice(list,s+1)), index + 1), index
 end
 
-function merge(a,b)
+function merge(a,b, index)
 
-  
+  reaper.ShowConsoleMsg("\na: " .. #a .. " b: " .. #b)
   reaper.ShowConsoleMsg("\nA: ")
   printNotesArray(a)
+  breadCrumbsArray(a, b)
   reaper.ShowConsoleMsg("\nB: ")
   printNotesArray(b)
+  --breadCrumbsArray(b)
   
   return table.combine(a,b)
 end
 
-mergesort(notes)
+function table.clone(org)
+  return {table.unpack(org)}
+end
+
+function deepcopy(orig, copies)
+    copies = copies or {}
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        if copies[orig] then
+            copy = copies[orig]
+        else
+            copy = {}
+            copies[orig] = copy
+            for orig_key, orig_value in next, orig, nil do
+                copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
+            end
+            setmetatable(copy, deepcopy(getmetatable(orig), copies))
+        end
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
+function notePositionCorrection(array)
+    local offset = 0
+
+    for i = 1, #array do
+    
+    --reaper.ShowConsoleMsg(offset)
+    local startNote = offset
+    local endNote   = (array[i][5] - array[i][4]) + offset
+    offset = (endNote - startNote) + offset
+    
+    array[i][4] = startNote
+    array[i][5] = endNote
+
+    end
+
+  return array
+end
+
+output = mergesort(notes)
+
+reaper.ShowConsoleMsg(tostring(breadCrumbs))
+breadCrumbs = deepcopy(breadCrumbs)
+output = deepcopy(table.concat(breadCrumbs, output))
+output = deepcopy(notePositionCorrection(output))
+
+reaper.ShowConsoleMsg("\n")
+
+reaper.ShowConsoleMsg("\n")
+printNotesArray(output)
